66bda3db43d10b3b094a8af7e83f9109b8102afc
Bruce Schultz <bschultz013@gmail.com>
2018-04-18 19:38:02 +0200

    Combine cube functions into one, so now one function can output the loop numbers and incorporate a list if provided

--- cube_happy.py
+++ cube_happy.py
@@ -1,44 +1,40 @@
-def cube_happy(n):
+#Create a function that returns a list of Happy cubes
+def cube_happy(n, loopers = [0]):
+    '''
+    :param n: A Number to check whether it is a happy cube
+    :param loopers: A list of numbers that create an infinite loop and should be avoided
+    :return: If a loop forms, that number will be output, otherwise True
+    '''
+    repeats = loopers
     if n <= 0:
         raise ValueError("Input must be a positive value!")
-    k=500
+    k = 500 #Used if no loopers given
     split = list(str(n))
     ints = [j ** 3 for j in [int(i) for i in split]]
     sum_cubes = sum(ints)
-    while n != sum_cubes and k != 0:
-        split = list(str(sum_cubes))
-        ints = [j**3 for j in [int(i) for i in split]]
-        sum_cubes = sum(ints)
-        k -= 1
+    if n != sum_cubes and sum_cubes not in repeats:
+        while k != 0:
+            split = list(str(sum_cubes))
+            ints = [j ** 3 for j in [int(i) for i in split]]
+            sum_cubes = sum(ints)
+            k -= 1
     if sum_cubes == n:
         return True
     else:
         return sum_cubes
 
-# repeats = set()
-# for i in range(1, 1001):
-#     if cube_happy(i) is not True:
-#         repeats.add(cube_happy(i))
+#Quickly make a list of numbers that generate a loop
+loops = set()
+for i in range(1, 1001):
+    if cube_happy(i) is not True:
+        loops.add(cube_happy(i))
 
-def cubey(n):
-    repeats = [160, 1, 352, 407, 217, 133, 136, 919, 370, 371, 1459, 244, 55, 153, 250]
-    if n <= 0:
-        raise ValueError("Input must be a positive value!")
-    split = list(str(n))
-    ints = [j ** 3 for j in [int(i) for i in split]]
-    sum_cubes = sum(ints)
-    while n != sum_cubes and sum_cubes not in repeats:
-        split = list(str(sum_cubes))
-        ints = [j ** 3 for j in [int(i) for i in split]]
-        sum_cubes = sum(ints)
-    if sum_cubes == n:
-        return True
-    else:
-        return False
+print(loops)
 
+# Find all happy cubes from 1-1000
 cube_happy_numbers = []
 for i in range(1, 1001):
-    if cubey(i) is True:
+    if cube_happy(i, loopers=loops) is True:
         cube_happy_numbers.append(i)
 
 print("List of cube happy numbers:", cube_happy_numbers)
