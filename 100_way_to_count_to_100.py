e3cea89781ef1c27602658c4e28bf95434dc8b47
brucetony <bschultz013@gmail.com>
2018-04-18 16:41:01 +0200

    Finish 100 ways problem with annotations

--- 100_way_to_count_to_100.py
+++ 100_way_to_count_to_100.py
@@ -1,35 +1,26 @@
 import itertools
 
 def one_hundred():
-    pass
+    '''
+    A function that combines "123456789" in various ways using basic operators and evaluates synthesized equation
+    :return: number of combinations that equal 100
+    '''
+    operators = ["+", "-", "*", "/", ""]
+    operator_list = list(itertools.product(operators, repeat=8))
+    combo_counter = 0
+    for i in range(len(operator_list)):
+        equation = "1{}2{}3{}4{}5{}6{}7{}8{}9".format(*operator_list[i])
+        evaluation = eval(equation)
+        if evaluation == 100:
+            combo_counter += 1
+    print("There are {} ways to make these numbers equal 100".format(combo_counter))
 
-#Make a list of lists which contains all permutations of operators
-operator_list = []
-for i in range(1, 9):
-    operator_list.append([list(subset) for subset in itertools.combinations_with_replacement("+-*/", i)])
+one_hundred()
 
 
-#Make list with every combination of numbers
-nums_list = []
-nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-for i in range(len(nums)):
-    j = len(nums)
-    while i < j:
-        nums_list.append(''.join(nums[i:j]))
-        j -= 1
 
-nums_list.sort()
-print(nums_list)
 
-#Make list of lists of numbers that only have 1-9
-# combos_list = [list(combo) for combo in itertools.combinations(nums_list, 9)]
-# for i in range(len(nums_list)):
-#     if list(nums_list[i])[0] == "1":
-#         combo = [nums_list[i]]
-#         while int(list(combo[-1])[-1]) != 9:
-#             for test in nums_list[i+1:]:
-#                 if list(combo[-1])[-1] < list(test)[0]:
-#                     combo.append(test)
-#         combos_list.append((combo))
+
+
 
 
