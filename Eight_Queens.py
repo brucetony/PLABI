d7a9144b6924de3fe7fb80fef488985b86cd5377
Bruce Schultz <bschultz013@gmail.com>
2018-04-19 15:24:28 +0200

    Itertools makes np array into tuple

--- Eight_Queens.py
+++ Eight_Queens.py
@@ -2,14 +2,16 @@ import numpy as np
 import itertools as itt
 
 def queens(num_queens):
-    diag = np.diag(np.ones(num_queens))
+    diag = np.diag(np.ones(num_queens, dtype='int'))
+    diag_list = diag.tolist()
+    combos = [subset for subset in itt.permutations(diag_list)]
     pass
 
 diag = np.diag(np.ones(4, dtype='int'))
-diag = diag.tolist()
 test = itt.permutations(diag)
 
 combos = [subset for subset in test]
-print(combos)
+print(type(diag))
+print(type(combos[0]))
 
 #TODO use trace(matrix) to determine if queens are diagonal
