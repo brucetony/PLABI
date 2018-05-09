b0c317235f9e658a102c2f3e4cb915e9b45b2cd2
Bruce Schultz <bschultz013@gmail.com>
2018-05-09 13:26:35 +0200

    Add function to generate log odds scores

--- Scoring_matrices.py
+++ Scoring_matrices.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from math import log
 
 seqs = ["TSVKTYAKFVTH", "TSVKTYAKFSTH", "TSVKTYAKFVTH", "LSVKKYPKYVVQ", "SSVKKYPKYSVL"]
 
@@ -18,8 +19,8 @@ def AA_count(seq_list):
         AA_counts[AA] = all_AA.count(AA)
     return AA_counts
 
-def AA_freq(seq_list):
-    '''Return a dictionary with the frequencies for each amino acid in a list of sequences'''
+def AA_prob(seq_list):
+    '''Return a dictionary with the probabilities for each amino acid in a list of sequences'''
     abs_counts = AA_count(seq_list)
     AA_freqs = {}
     total_AA_count = len(''.join(seq_list))
@@ -27,9 +28,11 @@ def AA_freq(seq_list):
         AA_freqs[AA] = abs_counts[AA]/total_AA_count
     return AA_freqs
 
-def AA_pairs(seq_list):
+def AA_pair_freq(seq_list):
     '''Return a dictionary with absolute counts for every amino acid pair in a list of sequences'''
     pairings = list(combinations(seq_list, 2))
+
+    # Get absolute counts for each pair
     AA_pairs = dict()
     for pairs in pairings:
         for i in range(len(pairs[0])):
@@ -40,6 +43,24 @@ def AA_pairs(seq_list):
             else:
                 AA_pairs[test_pair] += 1
 
-    return AA_pairs
+    # Get freq of each pair
+    total_AA_pairs = sum(AA_pairs.values())
+    AA_pair_freqs = {key:(value/total_AA_pairs) for (key, value) in AA_pairs.items()}
 
-print(AA_pairs(seqs))
+    return AA_pair_freqs
+
+def AA_scores(seq_list):
+    '''Return a dictionary with the log odds score for each amino acid pair in a list of sequences'''
+    probs = AA_prob(seq_list)
+    pairs = AA_pair_freq(seq_list)
+
+    # Create a dictionary which uses the probabilites of the two amino acids and multiples by 2
+    AA_expected = {key:(2*probs[list(key)[0]]*probs[list(key)[1]]) for (key, value) in pairs.items()}
+
+    # Create dictionary of scores
+    scores = {key:int(round(2*log(pairs[key]/AA_expected[key],2))) for (key, value) in AA_expected.items()}
+
+    return scores
+
+a = AA_scores(seqs)
+print(a)
