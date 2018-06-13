2690cbd1cbea00c535e94974c9fce6dc3dbfffbc
brucetony <bschultz013@gmail.com>
2018-06-13 09:58:05 +0200

    Convert orf_finder to a generator and modify orf_writer to use generator input

--- ORFs_FASTA/orf_finder.py
+++ ORFs_FASTA/orf_finder.py
@@ -4,9 +4,13 @@ from fasta import single_fasta_sequence, write_fasta
 
 
 def orf_finder(seq):
+    """
+    Generator that yields the longest ORF positions
+    :param seq: DNA string containing 'ACTG'
+    :yield: tuple containing the start and stop positions within the sequence strand for the longest ORF
+    """
     seq = seq.upper()
     stop_codon_list = ['TAA', 'TAG', 'TGA']  # List of stop codons
-    longest_orfs = []  # List of orf tuples
     for start_pos in range(3):  # Start at position 0, 1, or 2
         k = start_pos
         start_codon = None
@@ -16,34 +20,27 @@ def orf_finder(seq):
                 k += 3
             elif seq[k:k+3] in stop_codon_list and start_codon is not None:
                 stop_codon = k+3
-                longest_orfs.append((start_codon, stop_codon))
+                yield start_codon, stop_codon
                 start_codon, stop_codon = None, None
                 k += 3
             else:
                 k += 3
 
-    return longest_orfs
-
 
 def orf_writer(seq, outfile, genome_identifier):
-    cDNAflag = False
     comp_strand = complement_strand(seq)  # Create complementary strand
-    orf_lists = [orf_finder(seq), orf_finder(comp_strand)]
     genome_identifier = genome_identifier.split(' ')[0]  # Used to removed long name after last '|' in name
-    for i in range(len(orf_lists)):
-        if i == 1:
-            cDNAflag = True
-        for orf in orf_lists[i]:
-            if cDNAflag:
-                header = '{}:c{}-{}'.format(genome_identifier, len(seq)-orf[0]+1, len(seq)-orf[1]+1)
-                write_fasta(outfile, header, comp_strand[orf[0]-1:orf[1]-1])
-            else:
-                header = '{}:{}-{}'.format(genome_identifier, orf[0], orf[1])
-                write_fasta(outfile, header, seq[orf[0]-1:orf[1]-1])
+    for orf in orf_finder(comp_strand):  # The complementary seq
+        header = '{}:c{}-{}'.format(genome_identifier, len(seq) - orf[0] + 1, len(seq) - orf[1] + 1)
+        write_fasta(outfile, header, comp_strand[orf[0] - 1:orf[1] - 1])
+    for orf in orf_finder(seq):
+        header = '{}:{}-{}'.format(genome_identifier, orf[0], orf[1])
+        write_fasta(outfile, header, seq[orf[0] - 1:orf[1] - 1])
 
 
-with open(sys.argv[1], 'r') as in_file:
-    hd, seq_of_interest = single_fasta_sequence(in_file)
+if __name__ == "__main__":
+    with open(sys.argv[1], 'r') as in_file:
+        hd, seq_of_interest = single_fasta_sequence(in_file)
 
-with open(sys.argv[2], 'w') as file:
-    orf_writer(seq_of_interest, file, hd)
+    with open(sys.argv[2], 'w') as file:
+        orf_writer(seq_of_interest, file, hd)
